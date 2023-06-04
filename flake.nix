70fb64bf7aacb7ab0aedca27fdea68d655a4b286
Ivan Petkov <ivanppetkov@gmail.com>
2023-06-04 20:37:12 +0000

    flake: update to nixos-23.05 (#14)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS support for pibox hardware";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     flake-compat = {
       url = "github:edolstra/flake-compat";
